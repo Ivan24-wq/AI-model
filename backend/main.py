4a12deeed34897ea92506de25de76b1ed42b674c
Ivan24-wq <iv8747583@gmail.com>
2026-04-27 18:37:29 +0300

    Code

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
-from api.routes import router
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
+from routers import pages
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = BASE_DIR / "frontend"
 
 app = FastAPI()
-app.include_router(router)
+
+app.mount(
+    "/frontend",
+    StaticFiles(directory=str(FRONTEND_DIR)),
+    name="frontend"
+)
+
+app.include_router(pages.router)
---
+++ backend/routers/pages.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from fastapi import APIRouter
+from fastapi.responses import FileResponse, JSONResponse
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+FRONTEND_DIR = BASE_DIR / "frontend"
+
+def safe_file(name: str):
+    file = FRONTEND_DIR / name
+    if not file.exists():
+        return JSONResponse(
+            status_code=400,
+            content={"error": f"{name} no found"}
+        )
+    return FileResponse(file)
+
+
+router = APIRouter()
+
+@router.get("/")
+def root():
+    return safe_file("register.html")
